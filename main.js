86cadd0c4a46a9892c01050d63918a4d4922148c
DavidJChpxich <razmik200145@gmail.com>
2023-04-24 23:57:27 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+let a
+console.log(a)
+a = 20
+console.log(a)
+console.log(b)
+var b = 14
---
+++ script.js
@@ -0,0 +1,98 @@
+/*
+let js = 'amazing';
+
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas")
+console.log(23)
+
+let fn = "Jonas"
+console.log(fn)
+// Variable name conventions //
+
+ */
+/*
+let js = true;
+console.log(js)
+
+console.log(typeof true);
+console.log(typeof 41);
+console.log(typeof "J uan");
+console.log(typeof js);
+
+
+ */
+
+/*
+let age = 31;
+age = 34;
+console.log(age);
+
+ */
+
+/*
+const now = 2023
+const ageRaz = now - 2001
+const age= now - 2005
+
+console.log(now - 1992 > now - 2012)
+
+
+const averageAge= (ageRaz + age) /2
+console.log(averageAge)
+
+
+
+const marksWeight = 78
+const marksHeight = 1.69
+
+const johnsWeight = 92
+
+
+ */
+
+/*
+const age = 25
+age = "Raz"
+
+console.log(age);
+
+
+ */
+
+
+/*
+if (1 || 0 && false && " ") {
+    console.log(1)
+} else if (0 || 1 || undefined) {
+    console.log(2)
+} else {
+    console.log(3)
+}
+*/
+
+// const massMark = 78;
+// const heightMark = 1.69;
+// const massJohn = 92;
+// const heightJohn = 1.95;
+
+// const massMark =95;
+// const heightMark = 1.88;
+// const massJohn = 85;
+// const heightJohn = 1.76;
+//
+// const BMIMark = massMark / heightMark ** 2;
+// const BMIjohn = massJohn / (heightJohn * heightJohn);
+// const markHigherBMI = BMIMark > BMIjohn
+// console.log(BMIMark, BMIjohn, markHigherBMI)
+
+
+
+
+
+
+
+
+
+
+
